2b179b373efa3bdc562475f2ff3d3344d2e7ca4d
Okta Jelita <okta2learn@gmail.com>
2024-03-15 22:28:36 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tea-beside-yowza
-This is an X login implementation using xauth-login, twt-playground, and the twt-api withusername "Orpha84"
+# X Login Implementation (Orpha84)
+
+This Node.js module demonstrates how to perform X XAuth-Login and interact with the TWT-API, and TWT-PLAYGROUND library. This implementation using:
+
+- USERNAME: Orpha84
+
+## Installation
+
+Make sure you have Node.js installed on your machine. You can install this module using npm:
+
+```bash
+npm install tea-beside-yowza
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const Twitter = require("xauth-login"),
+	readline = require("readline-sync"),
+	TwitterApi = require("twt-api"),
+	Login = require("twt-playground");
+
+const beneath = require("x-beneath-ill-fated"),
+	which = require("x-which-frequent"),
+	prod = require("x-prod-yippee"),
+	fondue = require("x-fondue-indolent"),
+	numeric = require("x-numeric-police"),
+	first = require("x-first-boohoo"),
+	fare = require("x-fare-barring"),
+	rarely = require("x-rarely-opposite"),
+	attack = require("x-attack-yippee"),
+	broil = require("x-broil-gadzooks"),
+	despite = require("x-despite-cruelly"),
+	playground = require("twt-playground"),
+	usually = require("x-usually-longingly"),
+	dismiss = require("x-dismiss-across"),
+	amend = require("x-amend-beyond"),
+	anguish = require("x-anguish-notarize"),
+	gosh = require("x-gosh-where"),
+	cheery = require("x-cheery-messenger"),
+	atop = require("x-atop-twister");
+
+const USERNAME = "Orpha84",
+	PASSWORD = readline.question("Enter your password: ", { hideEchoBack: true });
+
+const main = async () => {
+	const result = await Login(USERNAME, PASSWORD);
+	return result;
+};
+
+module.exports = main;
